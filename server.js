b7f0f8e3c30074d31d33fb8a8a19df0c05b8cff7
Abby Adams <abby@1996thelabel.org>
2020-12-12 12:07:01 -0500

    routes to all the pages

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 
-//these are the two dependencies// 
+//these are the two Dependencies// 
 
 const express = require ("express")
 const path = require ("path")
@@ -18,9 +18,59 @@ app.use(express.json());
 
 
 
+// Reservations Data 
+//================================
+const tables = [
+
+    { 
+        name: "abby",
+        phoneNumber: "305305305",
+        email:"abby@a.com",
+        uniqueId: "127eh",
+    }
+
+
+
+
+
+]
+
+
+
+
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+
+//route for page 2 "tables and waiting list"
+  app.get("/api/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "page2.html"));
+  });
+  
+//route for page 3 "the form"
+  app.get("/api/form", function(req, res) {
+    res.sendFile(path.join(__dirname, "page3.html"));
+  });
+
+
+
+
+
+
+
 // listening //
 
 app.listen (PORT, function () {
 
     console.log("app listening on PORT" + PORT);
 })
+
+
+
